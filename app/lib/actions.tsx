f146078e34afa81fd235a832abfc43b44f155e99
AbrahamVidal88 <abraham.vidal.pat@gmail.com>
2024-11-27 14:33:52 -0500

    function delete invoices added

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -22,9 +22,53 @@ export async function createInvoice(formData: FormData){
       });
       const amountInCents = amount * 100;
       const date = new Date().toISOString().split('T')[0];
+    try{  
       await sql`INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amount}, ${status}, ${date})`;
+    }catch(error){
+      return{
+        message: 'Database error: Failed to create invoice.',
+      };
+    }
 
   revalidatePath('/dashboard/invoices'); 
   redirect('/dashboard/invoices');   
+}
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+// ...
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ try{
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ }catch(error){
+  return{
+    message:'Database Error: Failed to update invoice.',
+  };
+ }
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+export async function deleteInvoice(id: string) {
+  try{
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+  return {message:'Deleted invoice.'};
+  }catch(error){
+  return{
+    message:'Failed to delete invoice.',
+    }
+  }
 }
